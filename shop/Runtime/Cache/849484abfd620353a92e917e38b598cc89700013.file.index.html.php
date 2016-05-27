dc1ef57bb58845523e607e32ecc0fe107daad23a
hebly723 <hebly723@163.com>
2016-05-27 15:12:25 +0800

    mysql支持中文 	修改:         shop/Common/Conf/config.php 	修改:         shop/Common/Conf/config.php~ 	修改:         shop/Home/Controller/GoodsController.class.php~ 	修改:         shop/Home/View/Index/index.html 	修改:         shop/Home/View/Index/index.html~ 	修改:         shop/Runtime/Cache/849484abfd620353a92e917e38b598cc89700013.file.index.html.php 	修改:         shop/Runtime/Logs/Home/16_05_27.log 	修改:         shop/index.php~

--- shop/Runtime/Cache/849484abfd620353a92e917e38b598cc89700013.file.index.html.php
+++ shop/Runtime/Cache/849484abfd620353a92e917e38b598cc89700013.file.index.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.6, created on 2016-05-27 14:45:59
+<?php /* Smarty version Smarty-3.1.6, created on 2016-05-27 15:06:30
          compiled from "/usr/share/phpmyadmin/smallTest/shop/Home/View/Index/index.html" */ ?>
 <?php /*%%SmartyHeaderCode:6833492435747c6ad5e40d9-36823686%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '849484abfd620353a92e917e38b598cc89700013' => 
     array (
       0 => '/usr/share/phpmyadmin/smallTest/shop/Home/View/Index/index.html',
-      1 => 1464331552,
+      1 => 1464332679,
       2 => 'file',
     ),
   ),
@@ -168,7 +168,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 					<div class="shopDetail" id="math">
 						<dl class="shopDetail_item" >
 							<dt><a href="<?php echo @SEARCH_URL;?>
-math">数学</a><span class="badge hot pull-right"> HOT</span><img class="jiantou" src="<?php echo @IMG_URL;?>
+数学">数学</a><span class="badge hot pull-right"> HOT</span><img class="jiantou" src="<?php echo @IMG_URL;?>
 /icon/jiantou.jpg" height="8" width="5"></dt>
 							<dd><a href="#">高等数学</a><a href="#">线性代数</a><a href="#">离散数学</a></dd>
 						</dl>
